b369d30d4756fcbc00cf194f9e7d096f77d23ddd
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-07-18 13:19:37 +0300

    Remove unused code

--- run_setup.py
+++ run_setup.py
@@ -1,8 +1,6 @@
-# from app.v1.models.db_connect import db
-
 from flask_api import FlaskAPI
-from flask_sqlalchemy import SQLAlchemy
-import os, binascii
+import os
+import binascii
 
 from instance.config import app_config
 
@@ -10,8 +8,8 @@ app = FlaskAPI(__name__, instance_relative_config=True)
 
 secret = binascii.hexlify(os.urandom(24))
 
+
 def create_app(config_name):
-    # from app.v1.views.decorators import login_required
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -20,10 +18,8 @@ def create_app(config_name):
         "title": "BOOK-A-MEAL",
         "version": 1,
     }
-    # with app.app_context():
-    #     db.init_app(app)
-    #     db.create_all()
 
     return app
+
 
 app1 = create_app("development")
