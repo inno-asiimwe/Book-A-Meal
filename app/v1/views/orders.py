bf6d7be44c16b10c787fbd0693c467eeb0b8e474
Ronald <36371394+mutebironald4@users.noreply.github.com>
2018-07-19 11:52:21 +0300

    Add order route

--- app/v1/views/orders.py
+++ app/v1/views/orders.py
@@ -1,13 +1,16 @@
 from flask import jsonify, request, make_response, Blueprint
 from flasgger import swag_from
 from app.v1.views.decorators import login_required
-from app.v1.models.models import Order
+from app.v1.models.models import Order, order_schema
+from marshmallow import ValidationError
 
 
 orders = Blueprint('orders', __name__, url_prefix='/api/v1')
 
 
 @orders.route("/orders")
+@login_required
+@swag_from('api_doc/get_all_orders.yml')
 def get_all_orders():
     orders = Order.get_all_orders()
     if orders:
@@ -26,6 +29,8 @@ def get_all_orders():
 
 
 @orders.route("/orders/<int:id>", methods=["DELETE"])
+@login_required
+@swag_from('api_doc/delete_order.yml')
 def remove_order(id):
     order = Order.query.filter_by(id=id).first()
     if not order:
@@ -35,3 +40,17 @@ def remove_order(id):
     response = make_response(
         "The order has been deleted", 200)
     return response
+
+
+@orders.route("/orders/", methods=["POST"])
+@login_required
+@swag_from('api_doc/setup_order.yml')
+def account_create_order():
+    """Enables order setup"""
+    data = request.data
+    try:
+        order_data = order_schema.load(data)
+    except ValidationError as e:
+        return jsonify(e.messages)
+    id = order_data["menu_id"]
+    return Order.setup_order(id)
